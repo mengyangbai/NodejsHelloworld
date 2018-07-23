78a9da7a088ddaed032411e861fa716b8e929920
mengyangbai <mengyangbainju@gmail.com>
2018-07-23 15:12:12 +1000

    for 3.0.0

--- auth.js
+++ auth.js
@@ -1,15 +1,15 @@
 var passport = require("passport");
-import {
+let {
     Strategy,
     ExtractJwt
-} from "passport-jwt";
+} = require("passport-jwt");
 
 module.exports = app => {
     const Users = app.db.models.Users;
     const cfg = app.libs.config;
     const params = {
         secretOrKey: cfg.jwtSecret,
-        jwtFromRequest: ExtractJwt.fromAuthHeader()
+        jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme("jwt")
     };
     const strategy = new Strategy(params, (payload, done) => {
         Users.findById(payload.id)
